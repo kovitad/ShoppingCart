5f58fe59971ddf4a867e3c1302c6ce084dc66785
kovitad@gmail.com <kovitad@gmail.com>
2014-01-28 15:51:47 +1000

    Pusth

---
+++ src/com/kovitad/utils/ShowingApplet.java
@@ -0,0 +1,21 @@
+package com.kovitad.utils;
+
+import java.awt.Graphics;
+
+import javax.swing.JApplet;
+
+public class ShowingApplet extends JApplet {
+
+	private static final long serialVersionUID = -6821881841771813649L;
+
+	@Override
+	public void init() {
+
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		
+	}
+
+}
