0b67091dd1e9149c1645d5c79edbdee8e129914c
kovitad@gmail.com <kovitad@gmail.com>
2014-01-28 15:49:47 +1000

    commit

--- src/com/kovitad/utils/SortingUtils.java
+++ src/com/kovitad/utils/SortingUtils.java
@@ -35,32 +35,28 @@ public class SortingUtils {
 			int[] right = Arrays.copyOfRange(input, left.length, input.length);
 			// now divide again using recursive ...
 			merge(mergeSort(left), mergeSort(right), input);
-			
+
 		}
 		return input;
-	
 
 	}
 
-	
-	
-	
 	public static void merge(int[] left, int[] right, int[] input) {
-		
+
 		int leftSideIndex = 0;
-		
+
 		int rightSideIndex = 0;
-		
+
 		for (int i = 0; i < input.length; i++) {
-			
+
 			/*
-			System.out.println("left length:"+left.length);
-			System.out.println("leftSideIndex:"+leftSideIndex);
-			System.out.println("right length:"+right.length);
-			System.out.println("rightSideIndex:"+rightSideIndex);
-			*/
-			if (rightSideIndex >= right.length 
-					|| (leftSideIndex < left.length && left[leftSideIndex] <= right[rightSideIndex] )) {
+			 * System.out.println("left length:"+left.length);
+			 * System.out.println("leftSideIndex:"+leftSideIndex);
+			 * System.out.println("right length:"+right.length);
+			 * System.out.println("rightSideIndex:"+rightSideIndex);
+			 */
+			if (rightSideIndex >= right.length
+					|| (leftSideIndex < left.length && left[leftSideIndex] <= right[rightSideIndex])) {
 				// Take from left
 				input[i] = left[leftSideIndex];
 				leftSideIndex++;
@@ -69,33 +65,68 @@ public class SortingUtils {
 				input[i] = right[rightSideIndex];
 				rightSideIndex++;
 			}
-			
+
 			/*
-			if (rightSideIndex >= right.length || (leftSideIndex < left.length && 
-                    left[leftSideIndex] <= right[rightSideIndex])) {
-				input[i] = left[leftSideIndex];    // take from left
-				leftSideIndex++;
-            } else {
-            	input[i] = right[rightSideIndex];   // take from right
-                rightSideIndex++;
-            }*/
+			 * if (rightSideIndex >= right.length || (leftSideIndex <
+			 * left.length && left[leftSideIndex] <= right[rightSideIndex])) {
+			 * input[i] = left[leftSideIndex]; // take from left
+			 * leftSideIndex++; } else { input[i] = right[rightSideIndex]; //
+			 * take from right rightSideIndex++; }
+			 */
 
 		}
-		
+
+	}
+
+	/*
+	 * QuickSort
+	 * 
+	 * Best Case: o(nlogn) Average Case: o(nlogn) Worst Case: o(n^2)
+	 * 
+	 * Algorithm
+	 * 
+	 * QuickSort(A, StartIndex, EndIndex) 1: if StartIndex < EndIndex 2:
+	 * partitionIndex = partition(startIndex,endIndex); 3:
+	 * quickSort(startIndex,partitionIndex); 4:
+	 * quickSort(partitionIndex+1,A.length);
+	 * 
+	 * partition(startIndex, endIndex) 1: pivotIndex = endIndex- 1; 2: j =
+	 * startIndex-1; 3: for i=0 ;i * 3.1 if( i == endIndex -1) 3.1.1 j = j+1;
+	 * 3.1.2 swap(i,j);
+	 * 
+	 * 3.2 else if(arrayToSort[i]<= arrayToSort[pivotIndex]) 3.2.1 j = j+1;
+	 * 3.2.2 swap(i,j); 4: return j;
+	 */
+
+	public static void quickSort(int[] input) {
+		 quickSort(0,input.length,input);
+	}
+	
+	public static void quickSort(int startIndex, int endIndex, int [] input) {
+		 if(startIndex < endIndex){
+			 int partitionIndex = partitionIt(startIndex,endIndex,input);
+			 quickSort(startIndex,partitionIndex,input);
+			 quickSort(partitionIndex+1,input.length,input);
+        }
+	}
+
+	public static int partitionIt(int startIndex, int endIndex, int[] arrayToSort) {
+		int pivotIndex = endIndex - 1;
+		int j = startIndex - 1;
+		for (int i = startIndex; i < endIndex; i++) {
+			if (i == endIndex - 1) {
+				j = j + 1;
+				swapIt(arrayToSort, i, j);
+			} else if (arrayToSort[i] <= arrayToSort[pivotIndex]) {
+				j = j + 1;
+				swapIt(arrayToSort, i, j);
+			}
+		}
+		return j;
 	}
 
 	
-	public static void quickSort(int[] input) {
-		
-		int partitionIndex = 0;
-		
-		//divide and conquer for the partition
-		
-		
-		
-	}
-	
-	
+
 	/**
 	 * 
 	 * @param input
@@ -111,7 +142,7 @@ public class SortingUtils {
 	public static void main(String[] argv) {
 		int[] test = new int[] { 5, 11, 7, 2, 9, 1, 0, 6, 8, 3 };
 		// This O(n2)
-		mergeSort(test);
+		quickSort(test);
 		for (int i = 0; i < test.length; i++) {
 			System.out.println(test[i] + ",");
 		}
